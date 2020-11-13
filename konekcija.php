2bd39ce1cacbc3a8baf95a5e540773aedc1795f8
MilosBrkic <brkicmilos97@gmail.com>
2020-11-13 15:12:45 +0100

    izmena

--- konekcija.php
+++ konekcija.php
@@ -1,11 +1,14 @@
 <?php
 
-$mysql_server = "ec2-54-247-94-127.eu-west-1.compute.amazonaws.com";
-$mysql_user = "rhabgrdmqbmktd";
-$mysql_pass = "45ce463f833fa692c92c999d76e3fa90d6ede3341d92797e5f718656bddcf249";
-$mysql_db = "dfpi1aenr4h0j7";
+$mysql_server = getenv('DB_HOST');//"ec2-54-247-94-127.eu-west-1.compute.amazonaws.com";
+$mysql_user = getenv('DB_USER');//"rhabgrdmqbmktd";
+$mysql_pass = getenv('DB_PASS');//"45ce463f833fa692c92c999d76e3fa90d6ede3341d92797e5f718656bddcf249";
+$mysql_db = getenv('DB_Name');//"dfpi1aenr4h0j7";
 
-$baza = new mysqli($mysql_server,$mysql_user,$mysql_pass,$mysql_db);
+echo getenv('DB_HOST');
+
+$baza = pg_connect("host=$mysql_server port=5432 dbname=$mysql_db user=$mysql_user password=$mysql_pass");
+//$baza = new mysqli($mysql_server,$mysql_user,$mysql_pass,$mysql_db);
 $baza->set_charset("utf8"); 
 
 $sql = "CREATE TABLE TestTable (
